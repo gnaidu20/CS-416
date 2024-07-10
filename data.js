0f5d11065b42a98690c5ff392f595bbf99261e13
gnaidu20 <156680930+gnaidu20@users.noreply.github.com>
2024-07-10 12:27:05 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
-// to load the CSV data
-d3.csv("https://flunky.github.io/cars2017.csv").then(function(data) {
+// data.js
+d3.csv("cars2017.csv").then(function(data) {
     window.carData = data;
     console.log(data);
 });
